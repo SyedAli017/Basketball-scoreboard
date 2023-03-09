6854d8922c17f88876137d5907e265bb1301326d
SyedAli017 <sydali3103@gmail.com>
2023-03-10 02:00:06 +0530

    changed guest to away

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let homePointCount = document.getElementById("home-point-count");
-let guestPointCount = document.getElementById("guest-point-count");
+let awayPointCount = document.getElementById("away-point-count");
 
 let homePoints = 0;
-let guestPoints = 0;
+let awayPoints = 0;
 
 function homePoint1() {
   homePoints += 1;
@@ -19,17 +19,17 @@ function homePoint3() {
   homePointCount.textContent = homePoints;
 }
 
-function guestPoint1() {
-  guestPoints += 1;
-  guestPointCount.textContent = guestPoints;
+function awayPoint1() {
+  awayPoints += 1;
+  awayPointCount.textContent = awayPoints;
 }
 
-function guestPoint2() {
-  guestPoints += 2;
-  guestPointCount.textContent = guestPoints;
+function awayPoint2() {
+  awayPoints += 2;
+  awayPointCount.textContent = awayPoints;
 }
 
-function guestPoint3() {
-  guestPoints += 3;
-  guestPointCount.textContent = guestPoints;
+function awayPoint3() {
+  awayPoints += 3;
+  awayPointCount.textContent = awayPoints;
 }
